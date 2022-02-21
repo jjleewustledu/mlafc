0db7cca5949b771ecc2d7759246c3644cd9d2ccd
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2022-02-20 23:36:57 -0600

    removed atlasCode

--- src/+mlafc/AFCRegistry.m
+++ src/+mlafc/AFCRegistry.m
@@ -7,7 +7,6 @@ classdef AFCRegistry < handle
  	%% It was developed on Matlab 9.5.0.1067069 (R2018b) Update 4 for MACI64.  Copyright 2019 John Joowon Lee.
  	
     properties
-        atlasCode = 333
         betaT = 1
         bold_suffix = '_faln_dbnd_xr3d_atl_g7_bpss_resid'
         GMonly = true
